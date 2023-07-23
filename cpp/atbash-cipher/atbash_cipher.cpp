19e70f23ceb21a80cc2d2ad1e3bc5ad00c578591
Richard-m-j <richardj815165@gmail.com>
2023-07-23 10:20:39 +0530

    making grade

--- cpp/atbash-cipher/atbash_cipher.cpp
+++ cpp/atbash-cipher/atbash_cipher.cpp
@@ -12,7 +12,7 @@ namespace atbash_cipher
     }
     int count_failed_students(vector<double> student_scores)
     {
-        vector <int>rounded_scores = round_down_scores(student_scores);
+        vector<int> rounded_scores = round_down_scores(student_scores);
         int failed_students = 0;
         for (int i = 0; i < rounded_scores.size(); i++)
             if (rounded_scores[i] < 48)
@@ -28,21 +28,28 @@ namespace atbash_cipher
                 above_threshold.push_back(student_scores[i]);
         return above_threshold;
     }
-    int* letter_grade(int score)
+    int *letter_grade(int score)
     {
         int lower_bounds[4];
-        lower_bounds[0] = 48+1;
+        lower_bounds[0] = 48 + 1;
         lower_bounds[1] = lower_bounds[0] + score / 4;
         lower_bounds[2] = lower_bounds[1] + score / 4;
         lower_bounds[3] = lower_bounds[2] + score / 4;
         return lower_bounds;
     }
-    vector <string> student_ranking(vector<int> student_scores, vector<string> student_names)
+    vector<string> student_ranking(vector<int> student_scores, vector<string> student_names)
     {
-        vector <string> student_ranking;
-        for(int i=0;i<student_scores.size();i++)
-            student_ranking.push_back(to_string(i + 1)+ student_names[i]+ to_string(student_scores[i]));
+        vector<string> student_ranking;
+        for (int i = 0; i < student_scores.size(); i++)
+            student_ranking.push_back(to_string(i + 1) + student_names[i] + to_string(student_scores[i]));
+        return student_ranking;
     }
-    string perfect_score(vector<int> student_scores, vector<string> student_names+)
+    string perfect_score(vector<string> student_names, vector<int> student_scores)
+    {
+        for (int i = 0; i < student_scores.size(); i++)
+            if (student_scores[i] == 100)
+                return student_names[i];
+        return "";
+    }
 
 } // namespace atbash_cipher
